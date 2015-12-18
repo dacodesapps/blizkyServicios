f89cccb2ef290c162ba687484bf8c4da2c7033da
Pedro Pablo Marrufo Peon <p_marrufo94@hotmail.com>
2015-12-18 14:06:09 -0600

    Use los links que me dijo eric, para el login

--- BlizkyServicios/InicioViewController.m
+++ BlizkyServicios/InicioViewController.m
@@ -17,7 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+
 }
+
+-(UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleDefault;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
--- BlizkyServicios/LogInViewController.m
+++ BlizkyServicios/LogInViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LogInViewController.h"
+#import "AFNetworking.h"
 
 @interface LogInViewController () <UITextFieldDelegate>
 
@@ -22,11 +23,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self.navigationController.navigationBar setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys: [UIFont fontWithName:@"OpenSans-Bold" size:20], NSFontAttributeName,[UIColor whiteColor], NSForegroundColorAttributeName, nil]];
+    
     self.emailTextfield.delegate = self;
     self.passwordTextfield.delegate = self;
     
-    [self.serviceButton setBackgroundImage:[UIImage imageNamed:@""] forState:UIControlStateSelected];
-    [self.serviceButton setBackgroundImage:[UIImage imageNamed:@""] forState:UIControlStateNormal];
+    [self.serviceButton setBackgroundImage:[UIImage imageNamed:@"selected.png"] forState:UIControlStateSelected];
+    [self.serviceButton setBackgroundImage:[UIImage imageNamed:@"unselected.png"] forState:UIControlStateNormal];
+    [self.staffButton setBackgroundImage:[UIImage imageNamed:@"selected.png"] forState:UIControlStateSelected];
+    [self.staffButton setBackgroundImage:[UIImage imageNamed:@"unselected.png"] forState:UIControlStateNormal];
+    
+    self.serviceButton.selected = YES;
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,10 +43,80 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
+#pragma mark - Textfield Delegate
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return NO;
+}
+
+#pragma mark - SegmentedControl
+- (IBAction)staffButtonPressed:(id)sender {
+    if(self.serviceButton.selected) {
+        [UIView animateWithDuration:0.3 animations:^{
+            self.serviceButton.selected = NO;
+            self.staffButton.selected = YES;
+            return;
+        }];
+    }
+    self.serviceButton.selected = NO;
+    self.staffButton.selected = YES;
+}
+
+- (IBAction)serviceButtonPressed:(id)sender {
+    if(self.staffButton.selected) {
+        [UIView animateWithDuration:0.3 animations:^{
+            self.staffButton.selected = NO;
+            self.serviceButton.selected = YES;
+            return;
+        }];
+    }
+    self.staffButton.selected = NO;
+    self.serviceButton.selected = YES;
+}
+
+
+#pragma mark - Api
+
 - (IBAction)logInButtonPressed:(id)sender {
+    [self.emailTextfield resignFirstResponder];
+    [self.passwordTextfield resignFirstResponder];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
+    NSDictionary *parameters = @{@"email":self.emailTextfield.text,
+                                @"password":self.passwordTextfield.text
+                                };
+    
+    NSString *link = self.serviceButton.selected ? @"http://69.46.5.166:3002/api/Suppliers/login" :@"http://69.46.5.166:3002/api/Staff/login";
+    
+    [manager POST:link parameters: parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSDictionary *dct = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingAllowFragments error:nil];
+        NSLog(@"%@",dct);
+        [UIApplication sharedApplication].networkActivityIndicatorVisible=NO;
+        [UIAlertController alertControllerWithTitle:@"blizky" message:@"Success" preferredStyle:UIAlertControllerStyleAlert];
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error, id responseObject) {
+        NSLog(@"Error: %@", [error description]);
+        
+        [UIApplication sharedApplication].networkActivityIndicatorVisible=NO;
+        [UIAlertController alertControllerWithTitle:@"blizky" message:@"failure" preferredStyle:UIAlertControllerStyleAlert];
+    }];
+
 }
 
 - (IBAction)forgotYourPasswordButtonPressed:(id)sender {
+}
+
+- (IBAction)cancelButtonPressed:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 /*
